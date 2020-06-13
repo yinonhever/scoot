95a06dc7b2a962354714c041dd587127bb79727b
yinonhever <yinonehever@gmail.com>
2020-06-13 12:18:35 +0300

    adding faq accordions

--- index.js
+++ index.js
@@ -14,9 +14,31 @@ $(".header__nav-toggle").on("click", function () {
     }
 })
 
-$(".header__btn").on("click", function() {
+$(".header__btn").on("click", function () {
     $(".header__navigation").removeClass("open");
     $("body").removeClass("no-scroll");
     $(".header__nav-toggle").prop("src", "assets/icons/hamburger.svg");
 })
 
+
+// ANIMATING THE ITEMS INSIDE THE MOBILE MENU
+
+if ($(window).width() <= 700) {
+    var delay = 0.2;
+
+    $(".header__nav-link").each(function () {
+        $(this).css("transition", "all 0.4s " + delay + "s");
+        delay = delay + 0.2;
+    })
+
+    $(".header__btn").css("transition", "all 0.4s " + delay + "s");
+}
+
+
+// MANAGING THE ACCORDION
+
+$(".accordion__item:first-child").addClass("active");
+
+$(".accordion__top").click(function () {
+    $(this).parent().toggleClass("active");
+})
